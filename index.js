6ca3abf9ccb8f1110aa099e3d8406be23a42242c
Khushi-Mittal <khushimittal0587@gmail.com>
2024-12-04 23:48:48 +0530

    updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mongoDB=require("./db");
 mongoDB(process.env.BACKEND_URL);
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://treasurehunt-seven.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin","*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type,Accept"
